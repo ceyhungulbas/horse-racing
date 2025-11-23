561202eb81158317547f5528ff3e9b9df87afc5d
CeyhunGulbas <cetree61@gmail.com>
2025-11-23 14:23:49 +0300

    feat: add constants for colors, horse names, and round distances

---
+++ src/constants/horseNames.js
@@ -0,0 +1,24 @@
+export const HORSE_NAMES = [
+  "TRUE ANGEL",
+  "CASH IS KING",
+  "BERİD",
+  "HİLAL ZAMANI",
+  "KAMBOÇYALI",
+  "HARDOX",
+  "BAY NALÇAKAN",
+  "DUMAN",
+  "TILISIM",
+  "COOKIE",
+  "TM BATAILE",
+  "MAGIC BANK",
+  "HUNTER OF DEAT",
+  "GRAND SUN",
+  "NAMLIBİR",
+  "ERBAALI SADİ",
+  "KIZIM UTKUN",
+  "TEKİNHANIM",
+  "FEEL MY POWER",
+  "ADİGE PERİT",
+];
+
+// I added the horse names from TJK lol
